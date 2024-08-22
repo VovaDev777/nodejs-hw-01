a1de64a3d05955a9949eb46200e9cf2df6d724ac
VovaDev777 <polvovater@gmail.com>
2024-08-22 16:14:21 +0200

    1

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,9 +1,12 @@
-// import { PATH_DB } from '../constants/contacts.js';
+import { PATH_DB } from '../constants/contacts.js';
 import { createFakeContact } from '../utils/createFakeContact.js';
+import * as fs from 'node:fs/promises';
 
-const generateContacts = (number) => {
-    const contacts = Array(number).fill(0).map(() => createFakeContact());
-    console.log(contacts);
+const generateContacts = async (number) => {
+  const contacts = Array(number)
+    .fill(0)
+    .map(() => createFakeContact());
+  await fs.writeFile(PATH_DB, JSON.stringify(contacts, null, 2));
 };
 
 generateContacts(10);
